448f9d396c06e9238f6dccdf3443ad32b8bbfe87
Josef Šimánek <retro@ballgag.cz>
2013-03-06 14:22:57 +0100

    Pure Moped i18n backend. Naive implementation!

--- lib/i18n/backend/moped.rb
+++ lib/i18n/backend/moped.rb
@@ -1,43 +1,50 @@
+require 'i18n/backend/base'
+
 module I18n
   module Backend
     class Moped
-      attr_reader :collection
+      module Implementation
+        attr_accessor :collection
+        include Base, Flatten
 
-      def initialize(collection, options={})
-        @collection, @options = collection, options
-      end
+        def initialize(collection, options={})
+          @collection, @options = collection, options
+        end
 
-      def []=(key, value, options = {})
-        key = key.to_s
-        doc = {:_id => key, :value => value}
-        collection.find(:_id => key).remove_all
-        collection.insert(doc)
-      end
+        def available_locales
+          collection.find.distinct(:locale).map {|l| l.to_sym}
+        end
 
-      # alias for read
-      def [](key, options=nil)
-        if doc = collection.find(:_id => key.to_s).one
-          doc["value"].to_s
+        def store_translations(locale, data, options = {})
+          escape = options.fetch(:escape, true)
+          flatten_translations(locale, data, escape, false).each do |key, value|
+            collection.find(:key => key.to_s, :locale => locale.to_s).remove_all
+            doc = {:key => key.to_s, :value => value, :locale => locale.to_s}
+            collection.insert(doc)
+          end
+        end
+
+        protected
+        def lookup(locale, key, scope = [], options = {})
+          key = normalize_flat_keys(locale, key, scope, options[:separator])
+          result = collection.find(:key => /^#{Regexp.quote(key.to_s)}/).entries
+
+          if result.empty?
+            nil
+          elsif result.first["key"] == key.to_s
+            result.first["value"]
+          else
+            chop_range = (key.size + FLATTEN_SEPARATOR.size)..-1
+            result = result.inject({}) do |hash, r|
+              hash[r["key"].slice(chop_range)] = r["value"]
+              hash
+            end
+            result.deep_symbolize_keys
+          end
         end
       end
 
-      def keys
-        collection.find.to_a.collect{|h| h["_id"]}
-      end
-
-      def del(key)
-        collection.find({:_id => key}).remove_all
-      end
-
-      # Thankfully borrowed from Jodosha's redis-store
-      # https://github.com/jodosha/redis-store/blob/master/lib/i18n/backend/redis.rb
-      def available_locales
-        locales = self.keys.map { |k| k =~ /\./; $` }
-        locales.uniq!
-        locales.compact!
-        locales.map! { |k| k.to_sym }
-        locales
-      end
+      include Implementation
     end
   end
 end
